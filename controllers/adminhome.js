e3929e02656ac50b9a90912e202fea644cb0b53c
rashedul-alam <26365486+rashedul-alam@users.noreply.github.com>
2019-10-27 16:14:07 +0600

    admin add user and scout done.
    admin add user and scout done.

--- controllers/adminhome.js
+++ controllers/adminhome.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var db = require('./../models/db');
-
+var userModel = require('./../models/user-model');
 var router = express.Router();
 
+router.get('*', function(request, response, next) {
+
+    if (request.cookies['type'] == "admin") {
+        console.log("admin granted.")
+        next();
+    } else {
+        response.redirect('/logout');
+    }
+
+});
 router.get('/', function(request, response) {
 
     if (request.cookies['username'] != null) {
@@ -11,5 +21,31 @@ router.get('/', function(request, response) {
         response.redirect('/logout');
     }
 });
+router.get('/adduser', function(request, response) {
+    console.log("1");
+    response.render('adminhome/adduser');
+});
+
+router.post('/adduser', function(request, response) {
+    console.log("2");
+    var user = {
+        username: request.body.username,
+        password: request.body.password,
+        type: request.body.type,
+        personalinfo: request.body.personalinfo
+    };
+    userModel.insert(user, function(status) {
+        if (status) {
+
+            response.render('adminhome/index');
+        } else {
+            response.redirect('adminhome/adduser');
+        }
+    });
+
+});
+
+
+
 
 module.exports = router;
